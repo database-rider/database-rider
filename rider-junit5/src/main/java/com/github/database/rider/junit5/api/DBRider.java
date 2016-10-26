2493f97f7d32fab3b41a854c3136ec06206bbb9f
rmpestano <rmpestano@gmail.com>
2016-10-26 16:35:37 -0200

    fixes #11

---
+++ rider-junit5/src/main/java/com/github/database/rider/junit5/api/DBRider.java
@@ -0,0 +1,22 @@
+package com.github.database.rider.junit5.api;
+
+import com.github.database.rider.junit5.DBUnitExtension;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import java.lang.annotation.*;
+
+/**
+ * Created by rafael-pestano on 26/10/2016.
+ *
+ * Shortcut to enable database rider in junit5 tests. Replaces @ExtendWith(DBUnitExtension.class) and can also be used at method level.
+
+ */
+@Target({ ElementType.METHOD, ElementType.TYPE })
+@Retention(RetentionPolicy.RUNTIME)
+@ExtendWith(DBUnitExtension.class)
+@Test
+public @interface DBRider {
+
+
+}
---
+++ rider-junit5/src/test/java/com/github/database/rider/junit5/DBRiderAnnotationIt.java
@@ -0,0 +1,32 @@
+package com.github.database.rider.junit5;
+
+import com.github.database.rider.core.api.connection.ConnectionHolder;
+import com.github.database.rider.core.api.dataset.DataSet;
+import com.github.database.rider.core.util.EntityManagerProvider;
+import com.github.database.rider.junit5.api.DBRider;
+import com.github.database.rider.junit5.model.User;
+import org.junit.jupiter.api.Test;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.runner.RunWith;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Created by pestano on 07/09/16.
+ */
+@RunWith(JUnitPlatform.class)
+public class DBRiderAnnotationIt {
+
+    private ConnectionHolder connectionHolder = () -> //<3>
+            EntityManagerProvider.instance("junit5-pu").connection();
+
+
+    @DBRider
+    @DataSet(value = "usersWithTweet.yml")
+    public void shouldListUsers() {
+        List<User> users = EntityManagerProvider.em().createQuery("select u from User u").getResultList();
+        assertThat(users).isNotNull().isNotEmpty().hasSize(2);
+    }
+}
