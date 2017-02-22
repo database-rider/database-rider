07b20e5d71a0c206fe136c3dfd1cf7d732c1c8dc
Osipov Artemy <osipov.artemy@gmail.com>
2017-02-22 11:07:01 +0300

    test for lowercase table

---
+++ rider-core/src/test/java/com/github/database/rider/core/model/lowercase/Tweet.java
@@ -0,0 +1,84 @@
+package com.github.database.rider.core.model.lowercase;
+
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+import java.util.Calendar;
+
+@Entity
+@Table(name = "\"tweet\"")
+public class Tweet {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "\"id\"")
+    private String id;
+
+    @Size(min = 1, max = 140)
+    @Column(name = "\"content\"")
+    private String content;
+
+    private Integer likes;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Calendar date;
+
+    @ManyToOne
+    User user;
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Integer getLikes() {
+        return likes;
+    }
+
+    public void setLikes(Integer likes) {
+        this.likes = likes;
+    }
+
+
+    public Calendar getDate() {
+        return date;
+    }
+
+    public void setDate(Calendar date) {
+        this.date = date;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Tweet tweet = (Tweet) o;
+
+        return !(id != null ? !id.equals(tweet.id) : tweet.id != null);
+
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+}
---
+++ rider-core/src/test/java/com/github/database/rider/core/model/lowercase/User.java
@@ -0,0 +1,90 @@
+package com.github.database.rider.core.model.lowercase;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "\"user\"")
+public class User {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "\"id\"")
+    private long id;
+
+    @Column(name = "\"name\"")
+    private String name;
+
+    @OneToMany(mappedBy = "user")
+    private List<Tweet> tweets;
+
+    @OneToMany(mappedBy = "followedUser")
+    private List<Follower> followers;
+
+    public User() {
+    }
+
+    public User(long id) {
+        this.id = id;
+    }
+
+    public User(long id, String name, String tweetId, String tweetContent, Integer tweetLikes) {
+        this.id = id;
+        this.name = name;
+        this.tweets = new ArrayList<>();
+        Tweet tweet = new Tweet();
+        tweet.setId(tweetId);
+        tweet.setContent(tweetContent);
+        tweet.setLikes(tweetLikes);
+        tweets.add(tweet);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Tweet> getTweets() {
+        return tweets;
+    }
+
+    public void setTweets(List<Tweet> tweets) {
+        this.tweets = tweets;
+    }
+
+    public List<Follower> getFollowers() {
+        return followers;
+    }
+
+    public void setFollowers(List<Follower> followers) {
+        this.followers = followers;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        return id == user.id;
+
+    }
+
+    @Override
+    public int hashCode() {
+        return name != null ? name.hashCode() : (int) id;
+    }
+}
