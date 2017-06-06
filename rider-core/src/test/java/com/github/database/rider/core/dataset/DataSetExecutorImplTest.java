300d896ffd55aafff9b549bce37a39a794e2bf74
jenskreidler <jenskreidler@yahoo.de>
2017-06-06 13:44:48 +0200

    Added DataSetExecutorImplTest.java

---
+++ rider-core/src/test/java/com/github/database/rider/core/dataset/DataSetExecutorImplTest.java
@@ -0,0 +1,36 @@
+package com.github.database.rider.core.dataset;
+
+import static org.junit.Assert.assertEquals;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import org.junit.Test;
+
+import com.github.database.rider.core.api.connection.ConnectionHolder;
+
+public class DataSetExecutorImplTest {
+
+    @Test
+    public void shouldReadScriptFromJarURL() throws MalformedURLException {
+        DataSetExecutorImpl dse = DataSetExecutorImpl.instance(new ConnectionHolder() {
+            private static final long serialVersionUID = 1L;
+
+            @Override
+            public Connection getConnection() throws SQLException {
+                return null;
+            }
+        });
+        // simulate URL resource read from jar file in classpath
+        URL resource = DataSetExecutorImpl.class.getResource("/scripts/users.jar");
+        resource = new URL("jar", "localhost", "file:" + resource.getFile() + "!/users/user-script.sql");
+        String[] statements = dse.readScriptStatements(resource);
+        assertEquals(4, statements.length);
+        assertEquals("DELETE FROM User", statements[0]);
+        assertEquals("INSERT INTO USER VALUES (10,'user10')", statements[1]);
+        assertEquals("INSERT INTO USER VALUES (20,'user20')", statements[2]);
+        assertEquals("INSERT INTO USER VALUES (30,'user30')", statements[3]);
+    }
+}
