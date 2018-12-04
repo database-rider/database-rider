555b07b8210bed763b4d779ffa0aea0794da5c8e
Markus Meisterernst <markus.meisterernst@gmail.com>
2018-12-04 21:46:56 +0100

    issue_105 fixed import for EntityManagerProvider

--- rider-core/src/main/java/com/github/database/rider/core/util/EntityManagerProvider.java
+++ rider-core/src/main/java/com/github/database/rider/core/util/EntityManagerProvider.java
@@ -17,7 +17,6 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 import java.sql.Connection;
-import java.util.Arrays;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
