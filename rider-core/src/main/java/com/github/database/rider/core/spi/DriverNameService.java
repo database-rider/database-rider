9a4688f18c6d2a449974d7bdac6f16de2dabc120
rafael-pestano <rafael-pestano@procergs.rs.gov.br>
2019-08-19 12:02:13 -0300

    refs #146 enhance javadoc

--- rider-core/src/main/java/com/github/database/rider/core/spi/DriverNameService.java
+++ rider-core/src/main/java/com/github/database/rider/core/spi/DriverNameService.java
@@ -1,12 +1,16 @@
 package com.github.database.rider.core.spi;
 
+import java.sql.Connection;
+
 /**
- * Used to the DBRider which driver it is working on.
+ * Used by DBRider to know which driver it is working on.
  *
  * By default DBRider will try to infer the name from connection metadata but if your driver name
  * does not match expected names you can implement his interface and enable it
  * in src/main/resources/META-INF/services/com.github.database.rider.core.spi.DriverNameService file
  * which may contain the fully qualified name of your DriverNameService implementation.
+ *
+ * @see com.github.database.rider.core.util.DriverUtils#getDriverName(Connection)
  */
 public interface DriverNameService {
 
