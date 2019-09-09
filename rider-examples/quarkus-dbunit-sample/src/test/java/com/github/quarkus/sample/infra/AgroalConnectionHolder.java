b51c68035ee0a081fc781d54b1012c2a031a1566
rmpestano <rmpestano@gmail.com>
2019-09-09 16:45:04 -0300

    perf

--- rider-examples/quarkus-dbunit-sample/src/test/java/com/github/quarkus/sample/infra/AgroalConnectionHolder.java
+++ rider-examples/quarkus-dbunit-sample/src/test/java/com/github/quarkus/sample/infra/AgroalConnectionHolder.java
@@ -2,15 +2,13 @@ package com.github.quarkus.sample.infra;
 
 import com.github.database.rider.cdi.JTAConnectionHolder;
 import io.agroal.api.AgroalDataSource;
-import io.agroal.api.configuration.supplier.AgroalDataSourceConfigurationSupplier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.annotation.PostConstruct;
 import javax.annotation.Priority;
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Alternative;
-import javax.sql.DataSource;
+import javax.inject.Inject;
 import java.sql.Connection;
 import java.sql.SQLException;
 
@@ -21,46 +19,36 @@ public class AgroalConnectionHolder extends JTAConnectionHolder {
 
     private static final Logger log = LoggerFactory.getLogger(AgroalConnectionHolder.class.getName());
 
-    private DataSource agroalDS;
+    private Connection connection;
 
-    @PostConstruct
+    @Inject
+    AgroalDataSource agroalDS;
+
+    @Override
     public void init() {
-        if(agroalDS != null) {
-            return;
-        }
-        super.init();
-        AgroalDataSourceConfigurationSupplier poolConfig = new AgroalDataSourceConfigurationSupplier()
-                .metricsEnabled(false)
-                .connectionPoolConfiguration(cp -> cp
-                        .maxSize(10)
-                        .connectionFactoryConfiguration(cf -> {
-
-                                    try {
-                                        return cf.jdbcUrl(super.getConnection().getMetaData().getURL());
-                                    } catch (SQLException e) {
-                                        throw new RuntimeException("Could not create connection pool configuration", e);
-                                    }
-                                }
-
-                        )
-                );
-
-        try {
-            agroalDS = AgroalDataSource.from(poolConfig);
-        } catch (SQLException e) {
-            throw new RuntimeException("Could not create datasource from connection pool configuration", e);
-        }
     }
-
 
     @Override
     public Connection getConnection() {
         try {
-            return agroalDS.getConnection();
+            if(connection == null) {
+                connection = agroalDS.getConnection();
+            }
         } catch (SQLException e) {
             log.warn("Using default JTA connection.");
             //fallback to connection without pool
-            return super.getConnection();
+            super.init();
+            connection = super.getConnection();
         }
+        return connection;
     }
+
+    /*@Override
+    public void tearDown() {
+        try {
+            agroalDS.flush(AgroalDataSource.FlushMode.GRACEFUL);
+        } catch (Exception e) {
+            log.warn("Could not close current connection.", e);
+        }
+    }*/
 }
