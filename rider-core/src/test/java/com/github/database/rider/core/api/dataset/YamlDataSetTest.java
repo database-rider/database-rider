4387e8fc6e91dd29d3fdd272e954056a795a6086
MetallFoX <metallfox@list.ru>
2021-01-25 18:21:01 +0300

    Fix YamlDataSet table order
    Fixes: #255

--- rider-core/src/test/java/com/github/database/rider/core/api/dataset/YamlDataSetTest.java
+++ rider-core/src/test/java/com/github/database/rider/core/api/dataset/YamlDataSetTest.java
@@ -1,12 +1,13 @@
 package com.github.database.rider.core.api.dataset;
 
-import static org.junit.Assert.assertEquals;
-
-import org.junit.Test;
-
 import com.github.database.rider.core.api.configuration.DBUnit;
 import com.github.database.rider.core.api.configuration.Orthography;
 import com.github.database.rider.core.configuration.DBUnitConfig;
+import org.dbunit.dataset.DataSetException;
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
 
 @DBUnit
 public class YamlDataSetTest {
@@ -30,5 +31,15 @@ public class YamlDataSetTest {
                 dbUnitConfig);
 
         assertEquals("user", dataSet.applyCaseInsensitivity("uSeR"));
+    }
+
+    @Test
+    public void testPreserveTablesOrder() throws DataSetException {
+        DBUnitConfig dbUnitConfig = DBUnitConfig.from(YamlDataSetTest.class.getAnnotation(DBUnit.class));
+        // uses default config (dbunit.yml plus @DBUnit of this class)
+        YamlDataSet dataSet = new YamlDataSet(YamlDataSetTest.class.getResourceAsStream("/datasets/yml/user-with-datasets-order.yml"),
+                dbUnitConfig);
+
+        assertArrayEquals(new String[]{"USER", "USER_DETAILS"}, dataSet.getTableNames());
     }
 }
