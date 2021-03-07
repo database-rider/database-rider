2df67b5f747ba057cf11e465b67401628ff6dad9
rmpestano <rpestano@mobiquityinc.com>
2021-03-07 09:59:28 +0100

    refs #259 - add support @nested annotation for junit5

---
+++ rider-junit5/src/test/java/com/github/database/rider/junit5/DBUnitJUnit5NestedIt.java
@@ -0,0 +1,78 @@
+package com.github.database.rider.junit5;
+
+import com.github.database.rider.core.api.connection.ConnectionHolder;
+import com.github.database.rider.core.api.dataset.CompareOperation;
+import com.github.database.rider.core.api.dataset.DataSet;
+import com.github.database.rider.core.api.dataset.ExpectedDataSet;
+import com.github.database.rider.junit5.model.User;
+import com.github.database.rider.junit5.util.EntityManagerProvider;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.runner.RunWith;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(DBUnitExtension.class) //<1>
+@RunWith(JUnitPlatform.class) //<2>
+@DataSet(cleanBefore = true)
+public class DBUnitJUnit5NestedIt {
+
+    private ConnectionHolder connectionHolder = () -> //<3>
+            EntityManagerProvider.instance("junit5-pu").clear().connection();//<4>
+
+
+    @Test
+    @DataSet(value = "usersWithTweet.yml")
+    public void shouldListUsers() {
+        List<User> users = EntityManagerProvider.em().createQuery("select u from User u").getResultList();
+        assertThat(users).isNotNull().isNotEmpty().hasSize(2);
+    }
+
+    @Nested
+    class NestedTest {
+
+        @Test
+        @DataSet(cleanBefore = true)
+        public void shouldInsertUser() {
+            User user = new User();
+            user.setName("user");
+            user.setName("@rmpestano");
+            EntityManagerProvider.tx().begin();
+            EntityManagerProvider.em().persist(user);
+            EntityManagerProvider.tx().commit();
+            User insertedUser = (User) EntityManagerProvider.em().createQuery("select u from User u where u.name = '@rmpestano'").getSingleResult();
+            assertThat(insertedUser).isNotNull();
+            assertThat(insertedUser.getId()).isNotNull();
+        }
+    }
+
+    @Nested
+    class NestedExpectedDataSetTest {
+
+        @Test
+        @DataSet(value = "usersWithTweet.yml", transactional = true, cleanBefore = true)
+        @ExpectedDataSet(value = "expectedUser.yml", ignoreCols = "id")
+        public void shouldDeleteUser() {
+            User user = (User) EntityManagerProvider.em().createQuery("select u from User u  where u.id = 1").getSingleResult();
+            assertThat(user).isNotNull();
+            assertThat(user.getName()).isEqualTo("@realpestano");
+           EntityManagerProvider.em().remove(user.getTweets().get(0));
+            EntityManagerProvider.em().remove(user);
+        }
+    }
+
+    @Test
+    @DataSet(value = "usersWithTweet.yml",
+            useSequenceFiltering = false,
+            tableOrdering = {"USER", "TWEET"},
+            executeStatementsBefore = {"DELETE FROM TWEET", "DELETE FROM USER"}
+    )
+    public void shouldSeedDataSetUsingTableCreationOrder() {
+        List<User> users = EntityManagerProvider.em().createQuery("select u from User u left join fetch u.tweets").getResultList();
+        assertThat(users).hasSize(2);
+    }
+}
