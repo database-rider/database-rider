ba7b8a9d5c821918481714160ef53861e1fa76f4
Alexei Bratuhin <alexei.bratuhin@googlemail.com>
2021-09-06 20:49:52 +0200

    DBRIDER-358 = add scripting support in `@ExpectedDataSet`

--- rider-core/src/main/java/com/github/database/rider/core/api/dataset/ScriptableTable.java
+++ rider-core/src/main/java/com/github/database/rider/core/api/dataset/ScriptableTable.java
@@ -1,14 +1,11 @@
 package com.github.database.rider.core.api.dataset;
 
+import com.github.database.rider.core.api.scripting.ScriptEngineManagerWrapper;
 import org.dbunit.dataset.DataSetException;
 import org.dbunit.dataset.ITable;
 import org.dbunit.dataset.ITableMetaData;
 
 import javax.script.ScriptEngine;
-import javax.script.ScriptEngineManager;
-import javax.script.ScriptException;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -21,16 +18,13 @@ public class ScriptableTable implements ITable {
 
     private static final Logger log = Logger.getLogger(ScriptableTable.class.getName());
 
-    private final ScriptEngineManager manager;
-
-    private final Map<String, ScriptEngine> engines;
+    private final ScriptEngineManagerWrapper manager;
 
     private final ITable delegate;
 
     public ScriptableTable(ITable delegate) {
         this.delegate = delegate;
-        engines = new HashMap<>();
-        manager = new ScriptEngineManager();
+        manager = ScriptEngineManagerWrapper.getInstance();
     }
 
     @Override
@@ -46,56 +40,17 @@ public class ScriptableTable implements ITable {
     @Override
     public Object getValue(int row, String column) throws DataSetException {
         final Object value = delegate.getValue(row, column);
-        if (rowValueContainsScriptEngine(value)) {
-            ScriptEngine engine = getScriptEngine(value.toString().trim());
+        if (manager.rowValueContainsScriptEngine(value)) {
+            ScriptEngine engine = manager.getScriptEngine(value.toString().trim());
             if (engine != null) {
                 try {
-                    return getScriptResult(value.toString(), engine);
+                    return manager.getScriptResult(value.toString(), engine);
                 } catch (Exception e) {
                     log.log(Level.WARNING, String.format("Could not evaluate script expression for table '%s', column '%s'. The original value will be used.", getTableMetaData().getTableName(), column), e);
                 }
             }
         }
         return value;
-    }
-
-    private boolean rowValueContainsScriptEngine(Object value) {
-        if (value == null || value.toString().length() == 0) {
-            return false;
-        }
-        final String rowValueLowerCase = value.toString().toLowerCase();
-        return rowValueLowerCase.startsWith("js:") || rowValueLowerCase.startsWith("groovy:");
-    }
-
-    /**
-     * Parses table cell to get script engine
-     *
-     * @param value the table cell
-     * @return scriptEngine
-     */
-    private ScriptEngine getScriptEngine(String value) {
-        String engineName = value.substring(0, value.indexOf(':'));
-        if (engines.containsKey(engineName)) {
-            return engines.get(engineName);
-        } else {
-            ScriptEngine engine = manager.getEngineByName(engineName);
-            if (engine != null) {
-                engines.put(engineName, engine);
-            } else {
-                log.warning(String.format("Could not find script engine by name '%s'", engineName));
-            }
-            return engine;
-        }
-    }
-
-    /**
-     * Evaluates the script expression
-     *
-     * @return script expression result
-     */
-    private Object getScriptResult(String script, ScriptEngine engine) throws ScriptException {
-        String scriptToExecute = script.substring(script.indexOf(':') + 1);
-        return engine.eval(scriptToExecute);
     }
 
 }
