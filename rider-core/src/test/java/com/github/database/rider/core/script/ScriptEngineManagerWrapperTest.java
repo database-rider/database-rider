00e91a10a3e6df0d86a159aa1e8a388affe8226c
rmpestano <rpestano@mobiquityinc.com>
2021-09-12 09:22:53 +0200

    Adds a few more script assert tests

--- rider-core/src/test/java/com/github/database/rider/core/script/ScriptEngineManagerWrapperTest.java
+++ rider-core/src/test/java/com/github/database/rider/core/script/ScriptEngineManagerWrapperTest.java
@@ -1,5 +1,7 @@
 package com.github.database.rider.core.script;
 
+import org.assertj.core.api.SoftAssertions;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
@@ -7,15 +9,20 @@ import org.junit.rules.ExpectedException;
 import javax.script.ScriptException;
 
 import static org.assertj.core.api.Java6Assertions.assertThat;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
 
 
 public class ScriptEngineManagerWrapperTest {
+
     @Rule
     public ExpectedException exceptionRule = ExpectedException.none();
     private ScriptEngineManagerWrapper scriptEngineManager = ScriptEngineManagerWrapper.getInstance();
+    private SoftAssertions softly = new SoftAssertions();
 
+
+    @Before
+    public void init() {
+        softly = new SoftAssertions();
+    }
 
     @Test
     public void shouldGetJsScriptResult() throws ScriptException {
@@ -37,18 +44,47 @@ public class ScriptEngineManagerWrapperTest {
     }
 
     @Test
-    public void shouldGetJsScriptAssert() throws ScriptException {
+    public void shouldAssertValueGreaterThanZero() throws ScriptException {
         String script = "js:(value > 0)";
-        assertTrue(scriptEngineManager.getScriptAssert(script, 2));
-        assertFalse(scriptEngineManager.getScriptAssert(script, 0));
-        assertFalse(scriptEngineManager.getScriptAssert(script, -1));
+        softly.assertThat(scriptEngineManager.getScriptAssert(script, 2)).as("js script with value=2").isTrue();
+        softly.assertThat(scriptEngineManager.getScriptAssert(script, 0)).as("js script with value=0").isFalse();
+        softly.assertThat(scriptEngineManager.getScriptAssert(script, -1)).as("js script with value=-1").isFalse();
+        script = "groovy:(value > 0)";
+        softly.assertThat(scriptEngineManager.getScriptAssert(script, 2)).as("groovy script with value=2").isTrue();
+        softly.assertThat(scriptEngineManager.getScriptAssert(script, 0)).as("groovy script with value=0").isFalse();
+        softly.assertThat(scriptEngineManager.getScriptAssert(script, -1)).as("groovy script with value=-1").isFalse();
+        softly.assertAll();
     }
 
     @Test
-    public void shouldGetGroovyScriptAssert() throws ScriptException {
-        String script = "groovy:(value > 0)";
-        assertTrue(scriptEngineManager.getScriptAssert(script, 2));
-        assertFalse(scriptEngineManager.getScriptAssert(script, 0));
-        assertFalse(scriptEngineManager.getScriptAssert(script, -1));
+    public void shouldAssertNullValue() throws ScriptException {
+        SoftAssertions soft = new SoftAssertions();
+        String script = "js:(value == null)";
+        soft.assertThat(scriptEngineManager.getScriptAssert(script, null)).as("js script with null value").isTrue();
+        soft.assertThat(scriptEngineManager.getScriptAssert(script, 1)).as("js script with non-null value").isFalse();
+        script = "groovy:(value == null)";
+        soft.assertThat(scriptEngineManager.getScriptAssert(script, null)).as("groovy script with null value").isTrue();
+        soft.assertThat(scriptEngineManager.getScriptAssert(script, 1)).as("groovy script with non-null value").isFalse();
+        soft.assertAll();
+    }
+
+    @Test
+    public void shouldAssertContainsValue() throws ScriptException {
+        SoftAssertions soft = new SoftAssertions();
+        String script = "js:(value.contains('dbunit'))";
+        soft.assertThat(scriptEngineManager.getScriptAssert(script, "dbunit rules")).as("js script with 'dbunit rules' value").isTrue();
+        soft.assertThat(scriptEngineManager.getScriptAssert(script, "database rider rules")).as("js script 'database rider' value").isFalse();
+        script = "groovy:(value.contains('dbunit'))";
+        soft.assertThat(scriptEngineManager.getScriptAssert(script, "dbunit rules")).as("groovy script with 'dbunit rules' value").isTrue();
+        soft.assertThat(scriptEngineManager.getScriptAssert(script, "database rider rules")).as("groovy script 'database rider' value").isFalse();
+        soft.assertAll();
+    }
+
+    @Test
+    public void shouldNotAssertInvalidScript() throws ScriptException {
+        exceptionRule.expect(ScriptException.class);
+        exceptionRule.expectMessage("value.includes is not a function");
+        String script = "js:(value.includes('dbunit'))";
+        scriptEngineManager.getScriptAssert(script, "dbunit rules");
     }
 }
