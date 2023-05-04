6b0d31b0ec5f847555eec3e96f1c0ebf5cb26e5a
rmpestano <rmpestano@gmail.com>
2023-05-04 23:21:05 +0200

    refs #548

--- rider-core/src/main/java/com/github/database/rider/core/configuration/DBUnitConfig.java
+++ rider-core/src/main/java/com/github/database/rider/core/configuration/DBUnitConfig.java
@@ -13,19 +13,9 @@ import org.dbunit.database.IMetadataHandler;
 import org.dbunit.dataset.datatype.IDataTypeFactory;
 import org.yaml.snakeyaml.Yaml;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.InputStream;
 import java.lang.reflect.Method;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.EnumMap;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
-import java.util.Set;
+import java.util.*;
 
 import static com.github.database.rider.core.configuration.DBUnitConfigPropertyResolver.resolveProperties;
 import static com.github.database.rider.core.configuration.DBUnitConfigPropertyResolver.resolveProperty;
@@ -34,8 +24,6 @@ import static com.github.database.rider.core.configuration.DBUnitConfigPropertyR
  * represents DBUnit configuration of a dataset executor.
  */
 public class DBUnitConfig {
-
-    private static final List<String> ALLOWED_DATASET_FILE_NAMES = Arrays.asList("dbunit.yaml", "dbunit.yml");
 
     private String executorId;
     private Boolean cacheConnection;
@@ -125,19 +113,22 @@ public class DBUnitConfig {
     }
 
     public static DBUnitConfig fromCustomGlobalFile() {
-        return new BufferedReader(new InputStreamReader(Thread.currentThread().getContextClassLoader().getResourceAsStream("")))
-                .lines()
-                .filter(fileName -> ALLOWED_DATASET_FILE_NAMES.contains(fileName.toLowerCase()))
-                .map(fileName -> Thread.currentThread().getContextClassLoader().getResourceAsStream(fileName))
-                .filter(Objects::nonNull)
-                .map(inputStream -> {
-                    DBUnitConfig configFromFile = new Yaml().loadAs(inputStream, DBUnitConfig.class);
-                    configFromFile.initDefaultProperties();
-                    configFromFile.initDefaultConnectionConfig();
-                    return configFromFile;
-                })
-                .findFirst()
+        DBUnitConfig dbUnitConfig = Optional.ofNullable(Thread.currentThread().getContextClassLoader().getResourceAsStream("dbunit.yml"))
+                .map(DBUnitConfig::loadDbUnitConfig)
+                .orElse(DBUnitConfig.loadDbUnitConfig(Thread.currentThread().getContextClassLoader().getResourceAsStream("dbunit.yaml")));
+
+        return Optional.ofNullable(dbUnitConfig)
                 .orElse(new DBUnitConfig());
+    }
+
+    private static DBUnitConfig loadDbUnitConfig(InputStream inputStream) {
+        if (inputStream == null) {
+            return null;
+        }
+        DBUnitConfig configFromFile = new Yaml().loadAs(inputStream, DBUnitConfig.class);
+        configFromFile.initDefaultProperties();
+        configFromFile.initDefaultConnectionConfig();
+        return configFromFile;
     }
 
     public static DBUnitConfig from(DBUnit dbUnit) {
@@ -505,12 +496,12 @@ public class DBUnitConfig {
             SYSTEM_SCHEMAS.put(RiderDataSource.DBType.MSSQL, Collections.singleton("SYS"));
             SYSTEM_SCHEMAS.put(RiderDataSource.DBType.H2, Collections.singleton("INFORMATION_SCHEMA"));
             final Set<String> timescaledbSchemas = new HashSet<>(Arrays.asList(
-                "_timescaledb_cache",
-                "_timescaledb_catalog",
-                "_timescaledb_internal",
-                "_timescaledb_config",
-                "timescaledb_information",
-                "timescaledb_experimental"
+                    "_timescaledb_cache",
+                    "_timescaledb_catalog",
+                    "_timescaledb_internal",
+                    "_timescaledb_config",
+                    "timescaledb_information",
+                    "timescaledb_experimental"
             ));
             SYSTEM_SCHEMAS.put(RiderDataSource.DBType.TIMESCALEDB, Collections.unmodifiableSet(timescaledbSchemas));
 
